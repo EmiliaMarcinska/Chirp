ad2e79c00dc63b4fcf86f398bf51285e4a8ab162
EmiliaMarcinska <emilia.marcinska@gmail.com>
2026-02-12 18:55:53 +0100

    Android Application Convention Plugin

---
+++ build-logic/convention/src/main/kotlin/com/emarc/chirp/convention/ProjectExt.kt
@@ -0,0 +1,9 @@
+package com.emarc.chirp.convention
+
+import org.gradle.api.artifacts.VersionCatalog
+import org.gradle.api.artifacts.VersionCatalogsExtension
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.getByType
+
+val Project.libs: VersionCatalog
+    get() = extensions.getByType<VersionCatalogsExtension>().named("libs")
