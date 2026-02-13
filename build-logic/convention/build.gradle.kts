95f1b6dd50fc94bb8b856320f331c840616bf0d3
EmiliaMarcinska <emilia.marcinska@gmail.com>
2026-02-13 11:55:01 +0100

    CMP Feature Convention Plugin

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -49,5 +49,13 @@ gradlePlugin {
             id = "com.emarc.convention.kmp.library"
             implementationClass = "KmpLibraryConventionPlugin"
         }
+        register("cmpLibrary") {
+            id = "com.emarc.convention.cmp.library"
+            implementationClass = "CmpLibraryConventionPlugin"
+        }
+        register("cmpFeature") {
+            id = "com.emarc.convention.cmp.feature"
+            implementationClass = "CmpFeatureConventionPlugin"
+        }
     }
 }
