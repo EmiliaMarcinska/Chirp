28e032ad5912cb18e5fccfd343dcb33f0adcf442
EmiliaMarcinska <emilia.marcinska@gmail.com>
2026-03-08 12:37:07 +0100

    Triggering Forgot Password Emails

--- feature/auth/presentation/src/commonMain/kotlin/com/emarc/auth/presentation/forgot_password/ForgotPasswordState.kt
+++ feature/auth/presentation/src/commonMain/kotlin/com/emarc/auth/presentation/forgot_password/ForgotPasswordState.kt
@@ -6,7 +6,6 @@ import com.emarc.core.presentation.util.UiText
 
 data class ForgotPasswordState(
     val emailTextFieldState: TextFieldState = TextFieldState(),
-    val emailError: UiText? = null,
     val canSubmit: Boolean = false,
     val isLoading: Boolean = false,
     val errorText: UiText? = null,
