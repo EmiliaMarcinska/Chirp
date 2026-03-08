25f41b4574ccf80514f031daaa2a3d851d48a96d
EmiliaMarcinska <emilia.marcinska@gmail.com>
2026-03-08 13:14:34 +0100

    Clearing Focus On Tap

--- core/designsystem/src/commonMain/kotlin/com/emarc/core/designsystem/components/layouts/ChirpAdaptiveFormLayout.kt
+++ core/designsystem/src/commonMain/kotlin/com/emarc/core/designsystem/components/layouts/ChirpAdaptiveFormLayout.kt
@@ -30,6 +30,7 @@ import com.emarc.core.designsystem.components.brand.ChirpBrandLogo
 import com.emarc.core.designsystem.theme.ChirpTheme
 import com.emarc.core.designsystem.theme.extended
 import com.emarc.core.presentation.util.DeviceConfiguration
+import com.emarc.core.presentation.util.clearFocusOnTap
 import com.emarc.core.presentation.util.currentDeviceConfiguration
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
@@ -52,6 +53,7 @@ fun ChirpAdaptiveFormLayout(
         DeviceConfiguration.MOBILE_PORTRAIT -> {
             ChirpSurface(
                 modifier = modifier
+                    .clearFocusOnTap()
                     .consumeWindowInsets(WindowInsets.navigationBars)
                     .consumeWindowInsets(WindowInsets.displayCutout),
                 header = {
