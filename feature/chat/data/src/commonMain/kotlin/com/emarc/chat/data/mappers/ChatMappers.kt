d1f5c3663684c35678139fd54bf5be7347d12818
EmiliaMarcinska <emilia.marcinska@gmail.com>
2026-03-15 17:38:23 +0100

    Fetching a Single Chat

--- feature/chat/data/src/commonMain/kotlin/com/emarc/chat/data/mappers/ChatMappers.kt
+++ feature/chat/data/src/commonMain/kotlin/com/emarc/chat/data/mappers/ChatMappers.kt
@@ -2,10 +2,18 @@ package com.emarc.chat.data.mappers
 
 import com.emarc.chat.data.dto.ChatDto
 import com.emarc.chat.database.entities.ChatEntity
+import com.emarc.chat.database.entities.ChatInfoEntity
 import com.emarc.chat.database.entities.ChatWithParticipants
+import com.emarc.chat.database.entities.MessageWithSender
 import com.emarc.chat.domain.models.Chat
+import com.emarc.chat.domain.models.ChatInfo
+import com.emarc.chat.domain.models.ChatMessage
+import com.emarc.chat.domain.models.ChatMessageDeliveryStatus
+import com.emarc.chat.domain.models.ChatParticipant
 import kotlin.time.Instant
 
+typealias DataMessageWithSender = MessageWithSender
+typealias DomainMessageWithSender = com.emarc.chat.domain.models.MessageWithSender
 
 fun ChatDto.toDomain(): Chat {
     return Chat(
@@ -13,6 +21,18 @@ fun ChatDto.toDomain(): Chat {
         participants = participants.map { it.toDomain() },
         lastActivityAt = Instant.parse(lastActivityAt),
         lastMessage = lastMessage?.toDomain()
+    )
+}
+
+fun ChatEntity.toDomain(
+    participants: List<ChatParticipant>,
+    lastMessage: ChatMessage? = null
+): Chat {
+    return Chat(
+        id = chatId,
+        participants = participants,
+        lastActivityAt = Instant.fromEpochMilliseconds(lastActivityAt),
+        lastMessage = lastMessage
     )
 }
 
@@ -29,5 +49,22 @@ fun Chat.toEntity(): ChatEntity {
     return ChatEntity(
         chatId = id,
         lastActivityAt = lastActivityAt.toEpochMilliseconds()
+    )
+}
+
+fun DataMessageWithSender.toDomain(): DomainMessageWithSender {
+    return DomainMessageWithSender(
+        message = message.toDomain(),
+        sender = sender.toDomain(),
+        deliveryStatus = ChatMessageDeliveryStatus.valueOf(this.message.deliveryStatus)
+    )
+}
+
+fun ChatInfoEntity.toDomain(): ChatInfo {
+    return ChatInfo(
+        chat = chat.toDomain(
+            participants = this.participants.map { it.toDomain() }
+        ),
+        messages = messagesWithSenders.map { it.toDomain() }
     )
 }
--- feature/chat/domain/src/commonMain/kotlin/com/emarc/chat/domain/chat/ChatRepository.kt
+++ feature/chat/domain/src/commonMain/kotlin/com/emarc/chat/domain/chat/ChatRepository.kt
@@ -1,11 +1,15 @@
 package com.emarc.chat.domain.chat
 
 import com.emarc.chat.domain.models.Chat
+import com.emarc.chat.domain.models.ChatInfo
 import com.emarc.core.domain.util.DataError
+import com.emarc.core.domain.util.EmptyResult
 import com.emarc.core.domain.util.Result
 import kotlinx.coroutines.flow.Flow
 
 interface ChatRepository {
     fun getChats(): Flow<List<Chat>>
+    fun getChatInfoById(chatId: String): Flow<ChatInfo>
     suspend fun fetchChats(): Result<List<Chat>, DataError.Remote>
+    suspend fun fetchChatById(chatId: String): EmptyResult<DataError.Remote>
 }
